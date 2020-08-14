341295501d0dee9230083faa6bbe3bb2e59666ad
marcosdid <limafilhoguerreiro@outlook.com>
2020-08-14 20:56:59 -0300

    page ListTeacher successfully

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,40 +4,51 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: number
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars2.githubusercontent.com/u/60055989?s=460&u=e690f902b4003c72e754e1a7c4929fb1b4628b73&v=4"
-          alt="Marcos Alves"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
 
         <div>
-          <strong>Marcos Alves</strong>
-          <span>Quimica</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus,
-        dolorem
-        <br />
-        <br />
-        tenetur facilis debitis labore quaerat quisquam hic! Praesentium
-        dolorem, aut quidem, eos minus, dolore cupiditate veniam consectetur
-        architecto corporis voluptatum!
-      </p>
+      <p> {teacher.bio} </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,90</strong>
+          <strong>{teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          href={`https://wa.me/${teacher.whatsapp}`} 
+          type="button" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
